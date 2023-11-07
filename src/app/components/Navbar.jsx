a89acb5e9b56c126f4706d3d342043ec924da0bf
LizzieTrix <khalishmulyadi21@gmail.com>
2023-11-07 17:38:27 +0700

    add navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from "./MenuOverlay";
+
+const navLinks = [
+  {
+    path: "#about",
+    title: "About",
+  },
+  {
+    path: "#projects",
+    title: "Projects",
+  },
+  {
+    path: "#contact",
+    title: "Contact",
+  },
+];
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
-export default Navbar
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
+          LOGO
+        </Link>
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button onClick={() => setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button onClick={() => setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+              <XMarkIcon className="h-5 w-5" />
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {navbarOpen ? <MenuOverlay links={navLinks}/> : null}
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto py-4 px-12">
-      <div class="contianer">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
+      <div class="contianer mt-24 mx-auto px-12 py-4">
         <HeroSection />
       </div>
     </main>
