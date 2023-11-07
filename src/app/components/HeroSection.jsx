7d229ab5f0506fe23b6865fe3cc9b9295a7a7626
LizzieTrix <khalishmulyadi21@gmail.com>
2023-11-07 23:04:27 +0700

    fix hero image and add glass effect on navbar

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -47,13 +47,13 @@ const HeroSection = () => {
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#181815] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-gradient-to-tr from-blue-300 via-blue-500 to-blue-700 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/dev-profile-zep-2.png"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
               alt="dev-profile"
-              height={300}
-              width={300}
+              height={700}
+              width={700}
             />
           </div>
         </div>
